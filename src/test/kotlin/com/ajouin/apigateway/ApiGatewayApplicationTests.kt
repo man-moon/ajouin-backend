ff380870bb856e18d5d3da4e7a4e322e1e1502a8
moomani <moonman@kakao.com>
2024-03-05 05:00:11 +0900

    add test code to check if the configuration files exists

--- src/test/kotlin/com/ajouin/apigateway/ApiGatewayApplicationTests.kt
+++ src/test/kotlin/com/ajouin/apigateway/ApiGatewayApplicationTests.kt
@@ -1,13 +1,17 @@
 package com.ajouin.apigateway
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun verifyApplicationPropertiesFile() {
+		val resourceDirectory = File("src/main/resources")
+		val applicationProperties = File(resourceDirectory, "application.properties")
+		Assertions.assertTrue(applicationProperties.exists(), "application.properties 존재하지 않음")
 	}
-
 }
